0b5d1c07c70582e5dba53f23fe1cee367789a68e
cwinnard <ctwinnard@gmail.com>
2015-10-19 15:44:47 -0400

    Update URLWriter.java

--- URLWriter.java
+++ URLWriter.java
@@ -9,11 +9,7 @@ public class URLWriter {
 		}
 		
 		FileCreator.createNewHTMLFile();
-<<<<<<< HEAD
 		FileWriter urlWriter = new FileWriter("src/VocabHTMLTest.html", true);
-=======
-		FileWriter urlWriter = new FileWriter(new FileWriter("src/VocabHTMLTest.html", true));
->>>>>>> 9d9f0d5b5bbb08a85c7241d0165bf110e6b5fbb2
 		urlWriter.write(content);
 		urlWriter.close();
 	}
